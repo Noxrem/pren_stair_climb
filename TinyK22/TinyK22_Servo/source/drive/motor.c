38c773a21de77e48fcd185eea0355033661346da
Noxrem <skular.trix@hotmail.ch>
2020-12-15 17:08:00 +0100

    new functions
    ADD: getMotorFLT function
    ADD: setMotorDIR function
    ADD: setMotorSLP function
    ADD: Pin macros

--- TinyK22/TinyK22_Servo/source/drive/motor.c
+++ TinyK22/TinyK22_Servo/source/drive/motor.c
@@ -17,19 +17,9 @@
 #include "term.h"
 #include "util.h"
 #include <string.h>
+#include <stdbool.h>
 
 // https://ebldc.com/?p=86
-
-// complete the macros to switch a pin configuration from GPIO to FTM-Mode and vice versa
-#define MOTOR_RIGHT_A_PWM()           (PORTD->PCR[0] = PORT_PCR_MUX(4))  // PTD0[4]: FTM3_CH0
-#define MOTOR_RIGHT_A_GPIO()          (PORTD->PCR[0] = PORT_PCR_MUX(1))  // PTD0[1]: GPIO
-#define MOTOR_RIGHT_B_PWM()           (PORTE->PCR[5] = PORT_PCR_MUX(6))  // PTE5[6]: FTM3_CH0
-#define MOTOR_RIGHT_B_GPIO()          (PORTE->PCR[5] = PORT_PCR_MUX(1))  // PTE5[1]: GPIO
-
-#define MOTOR_LEFT_A_PWM()            (PORTD->PCR[1] = PORT_PCR_MUX(4))  // PTD1[4]: FTM3_CH1
-#define MOTOR_LEFT_A_GPIO()           (PORTD->PCR[1] = PORT_PCR_MUX(1))  // PTD1[1]: GPIO
-#define MOTOR_LEFT_B_PWM()            (PORTE->PCR[6] = PORT_PCR_MUX(6))  // PTE6[6]: FTM3_CH1
-#define MOTOR_LEFT_B_GPIO()           (PORTE->PCR[6] = PORT_PCR_MUX(1))  // PTE6[1]: GPIO
 
 static tCommandLineHandler clh;       // terminal command line handler
 static int8_t valueRight;
@@ -174,7 +164,75 @@ tError motorParseCommand(const char *cmd)
   return result;
 }
 
+/**
+ * Returns the GPIO PDOR value of the motor driver board FLT Pin
+ *
+ * @param[in] side
+ *   side of the motor: R or L
+ *
+ * @return
+ * 	 value of the pin
+ */
+bool getMotorFLT(char side)
+{
+	if(side == 'R')
+	{
+		return (MOTOR_R_FLT_PDIR & (uint32_t)(1 << MOTOR_R_FLT_PIN));	// return motor R fault pin value
+	}
+	else if(side == 'L')
+	{
+		return (MOTOR_L_FLT_PDIR & (uint32_t)(1 << MOTOR_L_FLT_PIN));	// return motor L fault pin value
+	}
+	else { return false; }	// error: invalid argument
+}
 
+/**
+ * Sets the GPIO PDOR value of the motor driver board DIR Pin
+ *
+ * @param[in] side
+ *   side of the motor: R or L
+ *
+ * @param[in] value
+ * 	 value to set the pin to
+ */
+void setMotorDIR(char side, bool value)
+{
+	if(side == 'R')
+	{
+		if(value) { MOTOR_R_DIR_PDOR |= (uint32_t)(1 << MOTOR_R_DIR_PIN); }	// set motor R direction pin
+		else { MOTOR_R_DIR_PDOR &= ~(uint32_t)(1 << MOTOR_R_DIR_PIN); }
+	}
+	else if(side == 'L')
+	{
+		if(value) { MOTOR_L_DIR_PDOR |= (uint32_t)(1 << MOTOR_L_DIR_PIN); }	// set motor L direction pin
+		else { MOTOR_L_DIR_PDOR &= ~(uint32_t)(1 << MOTOR_L_DIR_PIN); }
+	}
+	else { }	// error: invalid argument
+}
+
+/**
+ * Sets the GPIO PDOR value of the motor driver board SLP Pin
+ *
+ * @param[in] side
+ *   side of the motor: R or L
+ *
+ * @param[in] value
+ * 	 value to set the pin to
+ */
+void setMotorSLP(char side, bool value)
+{
+	if(side == 'R')
+	{
+		if(value) { MOTOR_R_SLP_PDOR |= (uint32_t)(1 << MOTOR_R_SLP_PIN); }	// set motor R sleep pin
+		else { MOTOR_R_SLP_PDOR &= ~(uint32_t)(1 << MOTOR_R_SLP_PIN); }
+	}
+	else if(side == 'L')
+	{
+		if(value) { MOTOR_L_SLP_PDOR |= (uint32_t)(1 << MOTOR_L_SLP_PIN); }	// set motor L sleep pin
+		else { MOTOR_L_SLP_PDOR &= ~(uint32_t)(1 << MOTOR_L_SLP_PIN); }
+	}
+	else { }	// error: invalid argument
+}
 
 /**
  * Initializes the motor driver:
@@ -188,22 +246,22 @@ tError motorParseCommand(const char *cmd)
  * - Motor L FLT: PTB3, GPIO, Mux:1
  * - Motor R PWM: PTD0, FTM3_CH0, Mux:4
  * - Motor L PWM: PTD1, FTM3_CH1, Mux:4
- * - Motor R DIR: PTC8, GPIO, Mux:1
- * - Motor L DIR: PTC9, GPIO, Mux:1
+ * - Motor R DIR: PTC8, GPIO, Mux:1			low: current from A to B, high: current from B to A
+ * - Motor L DIR: PTC9, GPIO, Mux:1			low: current from A to B, high: current from B to A
  * - Motor R SLP: PTC10, GPIO, Mux:1
  * - Motor L SLP: PTC11, GPIO, Mux:1
  */
 void motorInit(void)
 {
-	// Motor Fault Pin Inputs
+	// GPIO Direction
 	GPIOB->PDDR &= ~(1<<2 | 1<<3);	// configure PTB2 & PTB3 as input
 
-	// Output Pin
 	GPIOD->PDDR |= 1<<0 | 1<<1;					// configure PTD0 & PTD1 as output
 	GPIOC->PDDR |= 1<<8 | 1<<9 | 1<<10 | 1<<11;	// configure PTC8, PTC9, PTC10 & PTC11 as output
 
-	PORTB->PCR[2] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_PS(1);
-	PORTB->
+	// PORT as GPIO with pull-up
+	PORTB->PCR[2] = PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_PS(1);	// PTB2 as GPIO with pull-up
+	PORTB->PCR[3] = PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_PS(1);	// PTB3 as GPIO with pull-up
   // Configure the pin direction of the 4 pins as output.
   GPIOD->PDDR = 1<<0 | 1<<1;               // configure PTD0 & PTD1 as output
   GPIOE->PDDR = 1<<5 | 1<<6;               // configure PTE5 & PTE6 as output
