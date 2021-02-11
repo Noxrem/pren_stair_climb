b2d530c63aa957f06395f6c668aa4742f9ffbd07
Noxrem <skular.trix@hotmail.ch>
2021-02-11 11:47:57 +0100

    Update quad.c
    ADDED: EXT_GEAR_RATIO, this constant represents the ratio from the motor shaft to the actual wheel
    ADDED: the encoder functionality
    ADDED: encoder term commands

--- TinyK22/TinyK22_Servo/source/drive/quad.c
+++ TinyK22/TinyK22_Servo/source/drive/quad.c
@@ -21,16 +21,18 @@
 
 #define FTM_CLOCK                 250000    // 250 kHz
 #define FTM_PRESCALE              1         // div 1
-#define WHEEL_DIAMETER            6      	// 6 mm (old 19.2 mm)
-#define TICKS_PER_REVOLUTION      5756      // 5756 (old 142) ticks (counts) per wheel revolution (same as CPR "counts per revolution")
+#define WHEEL_DIAMETER            6      	// 6 mm
+#define EXT_GEAR_RATIO			  1			// Gear ration from the motor shaft to the actual wheel
+#define TICKS_PER_REVOLUTION      5756      // 5756 ticks (counts) per shaft revolution (same as CPR "counts per revolution")
 
-#define PERIODS_PER_REVOLUTION 	  (TICKS_PER_REVOLUTION / 4.0)                                     	// 1439 periods/revolution (old 35.50 periods/revolution) (same as PPR "pulses/periods per revolution")
-#define WHEEL_CIRCUMFERENCE       (WHEEL_DIAMETER * 3.141593)                                       // 18.85 mm (old 60.32 mm)
-#define NM_PER_TICK               (((10000000.0 * WHEEL_CIRCUMFERENCE / TICKS_PER_REVOLUTION)+5)/10)  // 1'042 Nanometer/Tick (old 424.788 => 425 Micrometer/Tick) (+5/10 is for rounding)
+																									  // Since there are 2 encoders (A,B) there is a factor of 4 ticks per period
+#define PERIODS_PER_REVOLUTION 	  (TICKS_PER_REVOLUTION / 4.0)                                     	  // 1439 periods/revolution (same as PPR "pulses/periods per revolution")
+#define WHEEL_CIRCUMFERENCE       (WHEEL_DIAMETER * 3.141593)                                         // 18.85 mm
+#define NM_PER_TICK               (((10000000.0 * WHEEL_CIRCUMFERENCE / TICKS_PER_REVOLUTION)+5)/10)  // 1'042 Nanometer/Tick (+5/10 is for rounding)
 
 // old velocity  = (19.2mm * PI * 250'000 * 4) / (142 * Ticks) = (19.2mm * PI * 250'000) / (35.5 * Ticks) = 424779/Ticks
-// velocity = (6mm * PI * 250'00 * 4) / (5756 * Ticks) = (6mm * PI * 250'000) / (1439 * Ticks) = 3'275/Ticks
-#define VELOCITY_PER_PERIOD       ((((uint32_t)(10 * WHEEL_CIRCUMFERENCE * FTM_CLOCK)) / (PERIODS_PER_REVOLUTION * FTM_PRESCALE)+5)/10) // 3'275
+// velocity = (6mm * PI * 250'00 * 4) / (5756 * Ticks) = (6mm * PI * 250'000) / (1439 * Ticks * External_Gear_Ratio) = 3'275/Ticks
+#define VELOCITY_PER_PERIOD       ((((uint32_t)(10 * WHEEL_CIRCUMFERENCE * FTM_CLOCK)) / (PERIODS_PER_REVOLUTION * FTM_PRESCALE * EXT_GEAR_RATIO)+5)/10) // 3'275
 
 #define QuadLeftA                 ((GPIOA->PDIR & (1<<13)) != 0) // FTM1_CH1
 #define QuadLeftB                 ((GPIOA->PDIR & (1<<12)) != 0) // FTM1_CH0
@@ -140,8 +142,8 @@ void FTM1_IRQHandler(void)
     }
   }
 
-  if (FTM1->CONTROLS[1].CnSC & FTM_CnSC_CHF_MASK) { // check for channel 1 interrupte
-    FTM1->CONTROLS[1].CnSC &= ~FTM_CnSC_CHF_MASK;   // clear interrupt fla
+  if (FTM1->CONTROLS[1].CnSC & FTM_CnSC_CHF_MASK) { // check for channel 1 interrupt
+    FTM1->CONTROLS[1].CnSC &= ~FTM_CnSC_CHF_MASK;   // clear interrupt flag
     if (QuadLeftA) {                                // rising edge:
       if (risingOkA) time = risingA;                //   if second or later call => period is valid
       risingA = 0;                                  //   reset period measuring time
@@ -238,7 +240,7 @@ void FTM2_IRQHandler(void)
 /**
  * Returns the velocity of the left wheel
  * @return
- *   the velocity in m/s
+ *   the velocity in mm/s
  */
 int16_t quadGetSpeedLeft(void)
 {
@@ -249,7 +251,7 @@ int16_t quadGetSpeedLeft(void)
 /**
  * Returns the velocity of the right wheel
  * @return
- *   the the velocity in m/s
+ *   the the velocity in mm/s
  */
 int16_t quadGetSpeedRight(void)
 {
