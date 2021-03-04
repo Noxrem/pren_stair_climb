9114765fc3e41eaec5f1e2e476a75a825d52b5ec
Manuel Gut <manuel.gut@stud.hslu.ch>
2021-03-04 05:17:32 +0100

    New StairDetection-Files added

--- Raspberry_Pi/Manuel_draft/StairDetection/Playground.py
+++ Raspberry_Pi/Manuel_draft/StairDetection/Playground.py
@@ -9,35 +9,37 @@ from skimage.transform import hough_line, hough_line_peaks
 
 cap = cv2.VideoCapture(0)
 
+
 def nothing(x):
     pass
 
 
-def slope(x1,y1,x2,y2):
+def slope(x1, y1, x2, y2):
     ###finding slope
-    if x2!=x1:
-        return((y2-y1)/(x2-x1))
+    if x2 != x1:
+        return ((y2 - y1) / (x2 - x1))
     else:
         return 'NA'
 
 
-def draw_line(image,x1,y1,x2,y2):
-    m=slope(x1,y1,x2,y2)
-    h,w=image.shape[:2]
-    if m!='NA':
+def draw_line(image, x1, y1, x2, y2):
+    m = slope(x1, y1, x2, y2)
+    h, w = image.shape[:2]
+    if m != 'NA':
         ### here we are essentially extending the line to x=0 and x=width
         ### and calculating the y associated with it
         ##starting point
-        px=0
-        py=-(x1-0)*m+y1
+        px = 0
+        py = -(x1 - 0) * m + y1
         ##ending point
-        qx=w
-        qy=-(x2-w)*m+y2
+        qx = w
+        qy = -(x2 - w) * m + y2
     else:
-    ### if slope is zero, draw a line with x=x1 and y=0 and y=height
-        px,py=x1,0
-        qx,qy=x1,h
+        ### if slope is zero, draw a line with x=x1 and y=0 and y=height
+        px, py = x1, 0
+        qx, qy = x1, h
     cv2.line(image, (int(px), int(py)), (int(qx), int(qy)), (0, 255, 0), 2)
+
 
 canny = "Canny"
 cv2.namedWindow(canny)
@@ -74,4 +76,3 @@ while True:
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
-
---
+++ Raspberry_Pi/Manuel_draft/StairDetection/StairDetection_3_withText.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+video = cv2.VideoCapture(0)
+
+# Write some Text
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (10, 100)
+fontScale = 1
+fontColor = (0, 255, 0)
+lineType = 2
+
+while True:
+    ret, orig_frame = video.read()
+    if not ret:
+        video = cv2.VideoCapture(0)
+        continue
+    frame = cv2.GaussianBlur(orig_frame, (5, 5), 0)
+    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # color1 = np.array([0, 0, 0])
+    # color2 = np.array([255, 255, 255])
+    # mask = cv2.inRange(hsv, color1, color2)
+    edges = cv2.Canny(frame, 200, 200)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, maxLineGap=150)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            if abs(x2 - x1) > 500 and abs(y2 - y1) < 30:
+                cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+                cv2.putText(frame, 'stair detected!',
+                            bottomLeftCornerOfText,
+                            font,
+                            fontScale,
+                            fontColor,
+                            lineType)
+    cv2.imshow("frame", frame)
+    cv2.imshow("edges", edges)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+video.release()
+cv2.destroyAllWindows()
