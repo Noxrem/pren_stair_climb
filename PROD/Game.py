e46c46e4523f3fbc25b10094135947ca5aadf83c
pi <gutmanuel@gmx.ch>
2021-03-25 10:30:42 +0100

    Adjust Game

--- PROD/Game.py
+++ PROD/Game.py
@@ -2,14 +2,14 @@ import Robot
 
 
 class Game:
-    robot = Robot.Robot("Gefyra")
+    robot = None
     distance_to_stair = None
     is_pictogram_detected = None
     is_stair_detected = None
 
     def __init__(self):
         print("create new game")
-        self.robot = Robot.Robot()
+        self.robot = Robot.Robot("Gefyra")
         self.is_pictogram_detected = False
         self.is_stair_detected = False
 
