66328f1b8775a18cba4f382d26113a7ff17cf332
Stephan Stofer <stephan.stofer@hslu.ch>
2021-04-03 16:44:58 +0200

    finish training

--- Raspberry_Pi/pictograms/pythonvideostream/server.py
+++ Raspberry_Pi/pictograms/pythonvideostream/server.py
@@ -14,13 +14,29 @@ server_socket.listen(0)
 connection = server_socket.accept()[0].makefile('rb')
 
 # loading the cascade classifier
+# hammer = "hammer"
+# ruler = "ruler"
+# hammer_path = "/Users/stofers/Development/HSLU/PREN/pren_stair_climb/Raspberry_Pi/pictograms/training/hammer/cascade.xml"
+# ruler_path = "/Users/stofers/Development/HSLU/PREN/pren_stair_climb/Raspberry_Pi/pictograms/training/ruler/cascade.xml"
 hammer = "hammer"
 ruler = "ruler"
-hammer_path = "/Users/stofers/Development/HSLU/PREN/pren_stair_climb/Raspberry_Pi/pictograms/training/hammer/cascade.xml"
-ruler_path = "/Users/stofers/Development/HSLU/PREN/pren_stair_climb/Raspberry_Pi/pictograms/training/ruler/cascade.xml"
+paintbucket = "paintbucket"
+pencil = "pencil"
+wrap = "wrap"
+wrench = "wrench"
+
+base_path ="/Users/stofers/Development/HSLU/PREN/pren_stair_climb/Raspberry_Pi/pictograms/training/"
+
+hammer_clsfr=cv2.CascadeClassifier(base_path+hammer+"/cascade.xml")
+ruler_clsfr=cv2.CascadeClassifier(base_path+ruler+"/cascade.xml")
+paintbucket_clsfr=cv2.CascadeClassifier(base_path+paintbucket+"/cascade.xml")
+pencil_clsfr=cv2.CascadeClassifier(base_path+pencil+"/cascade.xml")
+wrap_clsfr=cv2.CascadeClassifier(base_path+wrap+"/cascade.xml")
+wrench_clsfr=cv2.CascadeClassifier(base_path+wrench+"/cascade.xml")
+
 #item_clsfr = cv2.CascadeClassifier("../" + classifier + "/cascade.xml")
-hammer_clsfr = cv2.CascadeClassifier(hammer_path)
-ruler_clsfr = cv2.CascadeClassifier(ruler_path)
+#hammer_clsfr = cv2.CascadeClassifier(hammer_path)
+#ruler_clsfr = cv2.CascadeClassifier(ruler_path)
 try:
     img = None
     while True:
@@ -44,21 +60,50 @@ try:
         # converting the color image to a gray scale image
         hammers = hammer_clsfr.detectMultiScale(gray)
         rulers = ruler_clsfr.detectMultiScale(gray)
+        paintbuckets = paintbucket_clsfr.detectMultiScale(gray)
+        pencils = pencil_clsfr.detectMultiScale(gray)
+        wraps = wrap_clsfr.detectMultiScale(gray)
+        wrenchs = wrench_clsfr.detectMultiScale(gray)
         # detecting in the gray scale
-        # hammers is a 2D array containing n number of rows (n= number of hammers in the frame), 4 columns (x,y,w,h)
+        # hammers is a 2D array contaning n number of rows (n= number of hammers in the frame), 4 columns (x,y,w,h)
         for (x, y, w, h) in hammers:
             # going through each and assigning the x,y,w,h
 
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
             # Drawing a rectangle bounding the faces
-            cv2.putText(image, hammer, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.putText(img, hammer, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
         for (x, y, w, h) in rulers:
             # going through each and assigning the x,y,w,h
 
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
             # Drawing a rectangle bounding the faces
-            cv2.putText(image, ruler, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.putText(img, ruler, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        for (x, y, w, h) in paintbuckets:
+            # going through each and assigning the x,y,w,h
+
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # Drawing a rectangle bounding the faces
+            cv2.putText(img, paintbucket, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        for (x, y, w, h) in pencils:
+            # going through each and assigning the x,y,w,h
+
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # Drawing a rectangle bounding the faces
+            cv2.putText(img, pencil, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        for (x, y, w, h) in wraps:
+            # going through each and assigning the x,y,w,h
+
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # Drawing a rectangle bounding the faces
+            cv2.putText(img, wrap, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        for (x, y, w, h) in wrenchs:
+            # going through each and assigning the x,y,w,h
+
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # Drawing a rectangle bounding the faces
+            cv2.putText(img, wrench, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
         if img is None:
             img = pl.imshow(image)
--- Raspberry_Pi/pictograms/training/sources/echo_prep-training_commands.sh
+++ Raspberry_Pi/pictograms/training/sources/echo_prep-training_commands.sh
@@ -2,17 +2,29 @@
 
 BASE_PATH="/Users/stofers/Development/HSLU/PREN/pren_stair_climb/Raspberry_Pi/pictograms/training"
 SAMPLE_NAME=$1
+IDX=0
+INPUT_FORMAT=".jpg"
 
 if [ $# -eq 0 ]; then
     echo "No arguments provided, specify the name of the icon"
     exit 1
 fi
-
-echo "Collect all sample-files"
-echo "cd $BASE_PATH && find $SAMPLE_NAME -name '*.txt' | xargs -I{} cat {} > ${SAMPLE_NAME}_samples.txt"
+echo "move up"
+echo "  cd $BASE_PATH"
 
 echo "Collect negative sample data"
-echo "find ${SAMPLE_NAME}_negatives -name '*.jpg' > ${SAMPLE_NAME}_negatives.txt"
+echo "  find ${SAMPLE_NAME}_negatives -name '*.jpg' > ${SAMPLE_NAME}_negatives.txt"
+echo "\n"
+
+echo "Create positive samples"
+echo "  /Users/stofers/openCV/opencv/cmake-build-debug/bin/opencv_createsamples -info ${BASE_PATH}/$SAMPLE_NAME/${SAMPLE_NAME}_${IDX}/${SAMPLE_NAME}_${IDX}.txt -bg $BASE_PATH/${SAMPLE_NAME}_negatives.txt -img $BASE_PATH/sources/$SAMPLE_NAME/${SAMPLE_NAME}_${IDX}${INPUT_FORMAT} -num 1500 -w 48 -h 48 -maxxangle 0.0 -maxzangle 0.0 -maxyangle 0.0 -bgcolor 255"
+echo "\n"
+
+
+echo "Collect all sample-files"
+echo "  find $SAMPLE_NAME -name '*.txt' | xargs -I{} cat {} > ${SAMPLE_NAME}_samples.txt"
+echo "!!! ajust image path in ${SAMPLE_NAME}_samples.txt before running command above - use BBedit or similar tool !!!"
+echo "\n"
 #echo "find */hammer_0 -name '*.jpg' >> ${SAMPLE_NAME}_negatives.txt"
 #echo "find */paintbucket_0 -name '*.jpg' >> ${SAMPLE_NAME}_negatives.txt"
 #echo "find */pencil_0 -name '*.jpg' >> ${SAMPLE_NAME}_negatives.txt"
@@ -21,5 +33,8 @@ echo "find ${SAMPLE_NAME}_negatives -name '*.jpg' > ${SAMPLE_NAME}_negatives.txt
 #echo "find */wrench_0 -name '*.jpg' >> ${SAMPLE_NAME}_negatives.txt"
 
 echo "Create Vec-File"
-echo /Users/stofers/openCV/opencv/cmake-build-debug/bin/opencv_createsamples -info $BASE_PATH/${SAMPLE_NAME}_samples.txt -bg $BASE_PATH/${SAMPLE_NAME}_negatives.txt -vec $BASE_PATH/${SAMPLE_NAME}.vec -num 1500 -w 48 -h 48 -maxxangle 0.0 -maxzangle 0.0 -maxyangle 0.0 -bgcolor 255
+echo "  /Users/stofers/openCV/opencv/cmake-build-debug/bin/opencv_createsamples -info $BASE_PATH/${SAMPLE_NAME}_samples.txt -bg $BASE_PATH/${SAMPLE_NAME}_negatives.txt -vec $BASE_PATH/${SAMPLE_NAME}.vec -num 1500 -w 48 -h 48 -maxxangle 0.0 -maxzangle 0.0 -maxyangle 0.0 -bgcolor 255"
+echo "\n"
 
+echo "Run training"
+echo "  nohup /Users/stofers/openCV/opencv/cmake-build-debug/bin/opencv_traincascade -data ${SAMPLE_NAME} -vec ${SAMPLE_NAME}.vec -bg ${SAMPLE_NAME}_negatives.txt -numPos 1350 -numNeg 900 -numStages 7 -w 48 -h 48  -precalcValBufSize 2048 precalcIdxBufSize 2048 -mode ALL -numThreads 8 -minHitRate 0.999 -maxFalseAlarmRate 0.2 > ${SAMPLE_NAME}/log_${SAMPLE_NAME}_training.txt"
