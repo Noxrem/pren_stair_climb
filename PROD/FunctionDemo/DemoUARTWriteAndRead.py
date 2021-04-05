cdaa6c07228a1ea2c5efb3767f777e8de00d1c67
Manuel Gut <manuel.gut@stud.hslu.ch>
2021-04-05 10:11:18 +0200

    UART adapted

---
+++ PROD/FunctionDemo/DemoUARTWriteAndRead.py
@@ -0,0 +1,4 @@
+import UARTAccess
+
+access = UARTAccess.UARTAccess()
+access.write_and_read("drv help")
--- PROD/UARTAccess.py
+++ PROD/UARTAccess.py
@@ -5,18 +5,25 @@ class UARTAccess:
     access = None
 
     def __init__(self):
-        # TODO: delete first line & activate and adjust code
         print("create new UART access")
-        # self.access = "Access"
         self.access = serial.Serial("/dev/serial0", baudrate=57600, parity=serial.PARITY_NONE,
                                     stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 
     def write(self, message):
-        # TODO: activate code
         print("UART write")
         self.access.write(str.encode(message))
 
     def read(self):
-        # TODO: activate code
         print("UART read")
-        return self.access.read()
+        return self.access.readline()
+
+    def write_and_read(self, message):
+        print("UART write and read")
+        self.write(message)
+        is_data_available = True
+        while is_data_available:
+            line = self.read()
+            if line == "":
+                is_data_available = False
+            else:
+                print(line)
