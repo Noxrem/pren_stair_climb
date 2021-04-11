1d894a374eccb4abd27fc914ef1a7cd5f21fe228
Noxrem <skular.trix@hotmail.ch>
2021-04-11 17:20:46 +0200

    added defines for the align switches and implement init function

--- TinyK22/TinyK22_Servo/source/drive/bidge_align.h
+++ TinyK22/TinyK22_Servo/source/drive/bidge_align.h
@@ -14,6 +14,13 @@
 #ifndef SOURCES_DRIVE_BRIDGE_ALIGN_H_
 #define SOURCES_DRIVE_BRIDGE_ALIGN_H_
 
+#define SWITCH_RIGHT_PDIR GPIOD->PDIR
+#define SWITCH_RIGHT_PIN	1
+#define SWITCH_LEFT_PDIR	GPIOD->PDIR
+#define SWITCH_LEFT_PIN		2
+
+#define ALIGN_SPEED 20	// Speed for the alignment
+
 tError bridgeAlignParseCommand(const char *cmd);
 void bridgeAlignInit(void);
 
--- TinyK22/TinyK22_Servo/source/drive/bridge_align.c
+++ TinyK22/TinyK22_Servo/source/drive/bridge_align.c
@@ -12,13 +12,14 @@
  */
 
 #include <string.h>
+#include <stdbool.h>
 #include "platform.h"
 #include "term.h"
 #include "util.h"
 #include "drive.h"
 #include "quad.h"
 #include "motor.h"
-#include "bridge_align.h"
+#include "source/drive/bidge_align.h"
 
 static tCommandLineHandler clh;       // terminal command line handler
 
@@ -35,107 +36,34 @@ tError bridgeAlignParseCommand(const char *cmd)
   tError result = EC_INVALID_ARG;
   if (strcmp(cmd, "help") == 0)
   {
-    termWriteLine("drv (drive) commands:");
+    termWriteLine("aln (align) commands:");
     termWriteLine("  help");
-    termWriteLine("  setSpd [right(mm/s)] [left(mm/s)]");
-    termWriteLine("  setPIDParamR [kp 0..255] [ki 0..255] [kd 0..255]");
-    termWriteLine("  setPIDParamL [kp 0..255] [ki 0..255] [kd 0..255]");
-    termWriteLine("  status");
     result = EC_SUCCESS;
   }
   else if (strncmp(cmd, "setSpd", sizeof("setSpd")-1) == 0)
-    {
-      cmd += sizeof("setSpd");
-      int16_t drvSpdR;
-      int16_t drvSpdL;
-
-      result = utilScanDecimal16s(&cmd, &drvSpdR);		// parse the message to get the right motor speed
-      if(result != EC_SUCCESS) return result;
-
-      result = utilScanDecimal16s(&cmd, &drvSpdL);		// parse the message to get the left motor speed
-      if(result != EC_SUCCESS) return result;
-
-      driveSetSpeed(drvSpdL, drvSpdR);	// set the desired speed of the motors
-      result = EC_SUCCESS;
-    }
-  else if (strncmp(cmd, "setPIDParamR", sizeof("setPIDParamR")-1) == 0)
-  {
-    cmd += sizeof("setPIDParamR");
-    uint8_t kp;
-    uint8_t ki;
-    uint8_t kd;
-
-    result = parsePIDParam(cmd, &kp, &ki, &kd);
-    if(result != EC_SUCCESS) return result;
-    driveSetParametersR(kp, ki, kd);	// Set PID right parameters
-
-    char str[6];														// Return the received value for checking
-    termWrite("PIDR: ");
-    utilNum16sToStr(str, sizeof(str), kp);
-    termWrite(str);
-    termWrite(" ");
-    utilNum16sToStr(str, sizeof(str), ki);
-    termWrite(str);
-    termWrite(" ");
-    utilNum16sToStr(str, sizeof(str), kd);
-    termWriteLine(str);
-    result = EC_SUCCESS;
-  }
-  else if (strncmp(cmd, "setPIDParamL", sizeof("setPIDParamL")-1) == 0)
 	{
-		cmd += sizeof("setPIDParamL");
-    uint8_t kp;
-    uint8_t ki;
-    uint8_t kd;
+		cmd += sizeof("setSpd");
+		bool_t aligned = false;
 
-    result = parsePIDParam(cmd, &kp, &ki, &kd);
-		if(result != EC_SUCCESS) return result;
-		driveSetParametersL(kp, ki, kd);	// Set PID left parameters
-
-		char str[6];														// Return the received value for checking
-		termWrite("PIDL: ");
-		utilNum16sToStr(str, sizeof(str), kp);
-		termWrite(str);
-		termWrite(" ");
-		utilNum16sToStr(str, sizeof(str), ki);
-		termWrite(str);
-		termWrite(" ");
-		utilNum16sToStr(str, sizeof(str), kd);
-		termWriteLine(str);
+		driveSetSpeed(ALIGN_SPEED, ALIGN_SPEED);
+		while(aligned)
+		{
+			if(!(SWITCH_LEFT_PDIR ));	// TODO fertig implementieren
+		}
 		result = EC_SUCCESS;
 	}
-  else if (strcmp(cmd, "status") == 0)	// returns state of the motor values
-  {
-	  termWriteLine("drive status: ");
-	  termWrite("SetSpdR: ");							// return the set speed right
-	  termWriteNum16s(setValueRight);
-	  termWriteLine("");
-	  termWrite("SetSpdL: ");							// return the set speed left
-	  termWriteNum16s(setValueLeft);
-	  termWriteLine("");
-	  termWrite("PIDR: ");								// return the PID parameters right
-	  termWriteNum16s(kpR);
-	  termWrite(" ");
-	  termWriteNum16s(kiR);
-	  termWrite(" ");
-	  termWriteNum16s(kdR);
-	  termWriteLine("");
-	  termWrite("PIDL: ");								// return the PID parameters left
-	  termWriteNum16s(kpL);
-	  termWrite(" ");
-	  termWriteNum16s(kiL);
-	  termWrite(" ");
-	  termWriteNum16s(kdL);
-	  termWriteLine("");
-	  result = EC_SUCCESS;
-  }
   return result;
 }
 
 void bridgeAlignInit(void)
 {
+	// GPIO direction
+	GPIOD->PDDR &= ~(1<<1 | 1<<2);	// Configure PTA1 and PTA2 as input
 
+	// Set input with pull-up
+	PORTD->PCR[1] = PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_PS(1);	// PTA1 as GPIO with pull-up
+	PORTD->PCR[2] = PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_PS(1);	// PTA2 as GPIO with pull-up
 
   // register terminal command line handler
-  termRegisterCommandLineHandler(&clh, "drv", "(drive)", driveParseCommand);
+  termRegisterCommandLineHandler(&clh, "aln", "(align)", bridgeAlignParseCommand);
 }
