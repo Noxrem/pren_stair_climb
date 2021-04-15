3e023f95c46e83fa3a00f560c954d7240f400cc1
pi <gutmanuel@gmx.ch>
2021-04-15 14:23:48 +0200

    Integration Distance Sensor adapted

--- PROD/UltrasonicModuleControl.py
+++ PROD/UltrasonicModuleControl.py
@@ -19,7 +19,7 @@ class UltrasonicModuleControl:
 
     # Define sensor module 2
     sensor_side = None
-    GPIO_TRIGGER_2 = 17
+    GPIO_TRIGGER_2 = 23
     GPIO_ECHO_2 = 22
 
     # Define sensor list
@@ -28,14 +28,15 @@ class UltrasonicModuleControl:
     def __init__(self):
         logging.info("create new ultrasonic module control")
         self.sensor_front = UltrasonicModule("sensor_front", self.GPIO_ECHO_1, self.GPIO_TRIGGER_1)
-        self.sensor_side = UltrasonicModule("sensor_side", self.GPIO_ECHO_2, self.GPIO_TRIGGER_2)
+        self.sensor_side = UltrasonicModule("sensor_side", self.GPIO_ECHO_2, self.GPIO_TRIGGER_2) #TODO: sobald nicht auskommentiert, läuft Messung in UltrasonicModule nicht mehr, da
         self.sensor_list = [self.sensor_front, self.sensor_side]
 
-    def get_distance_in_cm(self):
-        nr_sensor = 0
-        return self.get_distance_mean(self.sensor_list.__getitem__(nr_sensor), self.NUMBER_MEASUREMENTS)
+    def get_distance_in_cm(self, number_sensor):
+        logging.debug("ultrasonic module control get distance in cm")
+        return self.get_distance_mean(self.sensor_list.__getitem__(number_sensor), self.NUMBER_MEASUREMENTS)
 
     def get_distance_mean(self, sensor, numbers_measurements):
+        logging.debug("ultrasonic moduule control get distance mean")
         values = numpy.empty([numbers_measurements])
         for count in range(numbers_measurements):
             values[count-1] = sensor.get_distance()
@@ -44,17 +45,19 @@ class UltrasonicModuleControl:
         return values.mean() - sensor.offset
 
     def calibrate_sensor(self, sensor, distance_sensor_to_object):
+        logging.debug("ultrasonic module control calibrate sensor")
         calibration_ongoing = True
         while calibration_ongoing:
             distance = self.get_distance_mean(sensor, self.NUMBER_MEASUREMENTS)
             sensor.offset = distance - distance_sensor_to_object
-            distance = self.get_distance_mean(sensor, self.NUMBER_MEASUREMENTS)
-            if distance - distance_sensor_to_object < 0.05:
+            if 0.3 > (distance - distance_sensor_to_object) > -0.3:
                 calibration_ongoing = False
                 logging.info(f"Calibration of sensor {sensor.name} successful: Offset = %.2f cm" % sensor.offset)
             else:
-                sensor.offset = 0
+                logging.warning(f"Calibration of sensor {sensor.name} not successful: Offset = %.2f cm" % sensor.offset)
+                sensor.offset = 0.00
 
     def calibrate_sensors(self, distance_sensor_to_object):
+        logging.debug("ultrasonic module control calibrate sensors")
         for sensor in self.sensor_list:
             self.calibrate_sensor(sensor, distance_sensor_to_object)
