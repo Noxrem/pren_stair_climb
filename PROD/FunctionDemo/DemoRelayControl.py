be450228f15970a5c4c126b31ea28636c67d0271
pi <gutmanuel@gmx.ch>
2021-04-16 11:45:07 +0200

    Integration RelayControl in MangetManager & create DemoRelayControl

---
+++ PROD/FunctionDemo/DemoRelayControl.py
@@ -0,0 +1,13 @@
+import time as t
+import RelayControl
+
+relay_control = RelayControl.RelayControl()
+
+while True:
+    for i in range(1, 5):
+        relay_control.set_on_relay(i)
+        print(f'Main: Is relay {i} on ' + str(relay_control.is_relay_on(i)))
+        t.sleep(1)
+        relay_control.set_off_relay(i)
+        print(f'Main: Is relay {i} on ' + str(relay_control.is_relay_on(i)))
+        t.sleep(1)
--- PROD/MagnetManager.py
+++ PROD/MagnetManager.py
@@ -1,42 +1,45 @@
 import logging
+import RelayControl
 
 class MagnetManager:
+    relay_control = None
     is_switched_on_magnet_bridge = None
     is_switched_on_magnet_socket = None
 
     def __init__(self):
         logging.info("create new MagnetManager")
+        self.relay_control = RelayControl.RelayControl()
         self.is_switched_on_magnet_socket = False
         self.is_switched_on_magnet_bridge = False
 
     def set_on_power_bridge(self):
         if not self.is_switched_on_magnet_bridge:
-            logging.info("set on power bridge")
+            logging.info("magnet manager: set on power bridge")
             self.is_switched_on_magnet_bridge = True
-            # TODO: Nathi: Implementation
+            self.relay_control.set_on_relay(1)
         else:
             logging.info("magnet for the bridge is already switched on")
 
     def set_off_power_bridge(self):
         if self.is_switched_on_magnet_bridge:
-            logging.info("set off power bridge")
+            logging.info("magnet manager: set off power bridge")
             self.is_switched_on_magnet_bridge = False
-            # TODO: Nathi: Implementation
+            self.relay_control.set_off_relay(1)
         else:
             logging.info("magnet for the bridge is already switched off")
 
     def set_on_power_socket(self):
         if not self.is_switched_on_magnet_socket:
-            logging.info("set on power socket")
+            logging.info("magnet manager: set on power socket")
             self.is_switched_on_magnet_socket = True
-            # TODO: Nathi: Implementation
+            self.relay_control.set_on_relay(2)
         else:
             logging.info("magnet for the socket is already switched on")
 
     def set_off_power_socket(self):
         if self.is_switched_on_magnet_socket:
-            logging.info("set off power socket")
+            logging.info("magnet manager: set off power socket")
             self.is_switched_on_magnet_socket = False
-            # TODO: Nathi: Implementation
+            self.relay_control.set_off_relay(2)
         else:
             logging.info("magnet for the socket is already switched off")
