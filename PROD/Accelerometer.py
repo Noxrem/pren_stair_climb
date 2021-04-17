404990a1d51c16d45a90cb4f5b462836c0262e23
Stephan Stofer <stephan.stofer@stud.hslu.ch>
2021-04-17 08:05:24 +0200

    add accelerometr

--- PROD/Accelerometer.py
+++ PROD/Accelerometer.py
@@ -1,10 +1,36 @@
 import logging
 
-class Accelerometer:
+import time
+import board
+import busio
+import adafruit_mpu6050
 
-    #  TODO: Nathi: tbd.
+class Accelerometer:
+    GPIO_SDA = None
+    GPIO_SCL = None
+    i2c = None
+    AcceleratorModule = None
 
     def __init__(self):
         logging.info("create new Accelerometer")
+        self.GPIO_SDA = 2
+        self.GPIO_SCL = 3
+
+        self.i2c = busio.I2C(GPIO_SCL, GPIO_SDA)
+        self.AcceleratorModule = adafruit_mpu6050.MPU6050(i2c)
 
 
+    def get_acceleration_x(self):
+        x_axes = 0
+        return self.AcceleratorModule.acceleration.__getitem__(x_axes)
+
+    def get_acceleration_y(self):
+        y_axes = 1
+        return self.AcceleratorModule.acceleration.__getitem__(y_axes)
+
+    def get_acceleration_z(self):
+        z_axes = 2
+        return self.AcceleratorModule.acceleration.__getitem__(z_axes)
+
+    def get_temperature_in_celsius(self):
+        return self.AcceleratorModule.temperature
