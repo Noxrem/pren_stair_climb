11a1273de15cf6f314e31146f121ead0ca78183c
Stephan Stofer <stephan.stofer@stud.hslu.ch>
2021-04-20 23:19:33 +0200

    lets get the party started :tada:

--- PROD/Speaker.py
+++ PROD/Speaker.py
@@ -1,52 +1,38 @@
+import os
+import time
+
 from gtts import gTTS
-from playsound import playsound
 import logging
 
 
 class Speaker:
-    texts = None
     celebration_sound = None
     language = None
 
     def __init__(self):
         logging.info("create new speaker")
-        self.texts = ["This is a hammer",
-                      "This is a wrap",
-                      "This is a ruler",
-                      "This is a paint bucket",
-                      "This is a pencil",
-                      "This is a wrench"]
         self.celebration_sound = "celebration.mp3"
         self.language = 'en'
 
     def play_text(self, found_pictogram_english_lowercase):
-        message = None
-        if found_pictogram_english_lowercase == "hammer":
-            message = self.texts[0]
-        elif found_pictogram_english_lowercase == "wrap":
-            message = self.texts[1]
-        elif found_pictogram_english_lowercase == "ruler":
-            message = self.texts[2]
-        elif found_pictogram_english_lowercase == "paint bucket":
-            message = self.texts[3]
-        elif found_pictogram_english_lowercase == "pencil":
-            message = self.texts[4]
-        elif found_pictogram_english_lowercase == "wrench":
-            message = self.texts[5]
-        slow = False
-        logging.info("Create MP3 file with pictogram text")
-        tts = gTTS(text=message, lang=self.language, slow=slow)
-        tts.save('TTS_message_pictogram.mp3')
+        message = "We can see a " + found_pictogram_english_lowercase + ", will immediately searching it!"
+        logging.info("Create MP3 file for pictogram %s", message)
+        tts = gTTS(text=message, lang=self.language, slow=False)
+        mp3 = 'TTS_message_pictogram.mp3'
+        tts.save(mp3)
+        os.system("omxplayer " + mp3)
         logging.info("Play MP3 file with pictogram text")
-        playsound('TTS_message_pictogram.mp3')
 
     def celebrate(self, found_pictogram_english_lowercase):
-        logging.info("celebrate")
-        message = "We have found the " + found_pictogram_english_lowercase
-        slow = False
-        tts = gTTS(text=message, lang=self.language, slow=slow)
-        tts.save('TTS_message_celebrate.mp3')
+        logging.info("!!!!celebrate!!!")
+        message = "We have found the target icon " + found_pictogram_english_lowercase
+        tts = gTTS(text=message, lang=self.language, slow=False)
+        mp3 = "TTS_message_celebrate.mp3"
+        tts.save(mp3)
         logging.info("Play MP3 file celebration")
-        playsound('TTS_message_celebrate.mp3')
-        playsound(self.celebration_sound)
-
+        os.system("omxplayer " + mp3)
+        logging.info("Party on!!!")
+        os.system("omxplayer " + self.celebration_sound + " &")
+        time.sleep(20)
+        logging.info("No drinks anymore.. so were gonna leave")
+        os.system("pkill omxplayer")
