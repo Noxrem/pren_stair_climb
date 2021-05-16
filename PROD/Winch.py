16bc42df6d566f9c9818dd82f3f4d510675f8b73
pi <gutmanuel@gmx.ch>
2021-05-16 18:22:54 +0200

    5 measurements to pull up / switch pull up direction

--- PROD/Winch.py
+++ PROD/Winch.py
@@ -15,14 +15,17 @@ class Winch:
 
     def pull_up(self, speed):
         logging.info(str(self.accelerometer.get_acceleration_x()))
-        message = "mot setW " + str(speed)
+        message = "mot setW -" + str(speed)
         self.serial_access.write(message)
         time.sleep(10)
-        while self.accelerometer.get_acceleration_x() < 9.81:  # TODO: define value
+        positiv_counter = 0
+        while positiv_counter < 5:
+            if self.accelerometer.get_acceleration_x() >= 9.81:  # TODO: define value
+                positiv_counter += 1
             message = self.accelerometer.get_acceleration_x()
             logging.info("pull up")
             logging.info(str(message))
-            time.sleep(0.5)
+            time.sleep(0.1)
         logging.info("pulled up")
         message = "mot setW 0"
         self.serial_access.write(message)
