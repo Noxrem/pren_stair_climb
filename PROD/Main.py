8073cdd3a3f5186374eed31273038023ce865d54
pi <gutmanuel@gmx.ch>
2021-05-16 10:03:02 +0200

    changes during Tests

--- PROD/Main.py
+++ PROD/Main.py
@@ -15,28 +15,28 @@ time.sleep(5)
 
 try:
 
-    robot.let_bridge_down()  # TODO: Only for test runs to save power
-    robot.let_socket_down()  # TODO: Only for test runs to save power
+    # robot.let_bridge_down()  # TODO: Only for test runs to save power
+    # robot.let_socket_down()  # TODO: Only for test runs to save power
 
-    robot.go_forward_and_stop_after_duration(70, Robot.calculate_duration_length_in_mm(70, 300))
+    #robot.go_forward_and_stop_after_duration(70, Robot.calculate_duration_length_in_mm(70, 300))
     robot.turn_and_find_pictogram(True)
     robot.speaker.play_text(robot.found_pictogram)
-    robot.turn_and_find_stair(False, True)
-    robot.go_forward_and_get_distance()
-    robot.do_alignment()
-    robot.go_to_drop_off_position()
-    robot.let_bridge_down()
-    time.sleep(20)
-    robot.go_forward_and_stop_after_duration(400, 1)
-    robot.go_backward_and_stop_after_duration(400, 1)
-    robot.let_socket_down()
-    robot.go_forward_fast()
-    robot.pull_up()
-    robot.pull_to_bridge_drop_off()
-    robot.turn_arm_down()
-    robot.go_forward_and_stop_after_duration(50, Robot.calculate_duration_length_in_mm(50, 400))  # todo: measure distance
-    robot.celebrate(robot.found_pictogram, time.time() - start_time)
-    robot.dispose()
+    # robot.turn_and_find_stair(False, True)
+    # robot.go_forward_and_get_distance()
+    # robot.do_alignment()
+    # robot.go_to_drop_off_position()
+    # robot.let_bridge_down()
+    # time.sleep(20)
+    # robot.go_forward_and_stop_after_duration(400, 1)
+    # robot.go_backward_and_stop_after_duration(400, 1)
+    # robot.let_socket_down()
+    # robot.go_forward_fast()
+    # robot.pull_up()
+    # robot.pull_to_bridge_drop_off()
+    # robot.turn_arm_down()
+    # robot.go_forward_and_stop_after_duration(50, Robot.calculate_duration_length_in_mm(50, 400))  # todo: measure distance
+    # robot.celebrate(robot.found_pictogram, time.time() - start_time)
+    # robot.dispose()
 
 except Exception as ex:
     logging.error("Error occurred. Programm is canceled:")
