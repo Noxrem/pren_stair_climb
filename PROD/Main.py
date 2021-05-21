570f8a59168938cb2001eb4c0a976521f03828e8
Manuel Gut <manuel.gut@stud.hslu.ch>
2021-05-21 12:00:35 +0200

    # "TODO: decide which methode should be used" added

--- PROD/Main.py
+++ PROD/Main.py
@@ -29,26 +29,26 @@ try:
     # robot.let_bridge_down()  # TODO: Only for test runs to save power
     # robot.let_socket_down()  # TODO: Only for test runs to save power
 
-    # robot.go_forward_and_stop_after_duration(70, Robot.calculate_duration_length_in_mm(70, 300))
-    # robot.turn_and_find_pictogram(True)
-    # robot.speaker.play_text(robot.found_pictogram)
-    robot.turn_and_find_stair_with_ultrasonic_and_camera_control(True)
-    # robot.go_forward_and_get_distance()
-    # robot.do_alignment()
-    # robot.go_to_drop_off_position()
-    # robot.let_bridge_down()
-    # robot.go_forward_and_stop_after_duration(50, 1)
-    # robot.go_forward_and_stop_after_duration(400, 1)
-    # robot.go_backward_and_stop_after_duration(400, 1)
-    # robot.go_backward_and_stop_after_duration(50, 1)
-    # robot.let_socket_down()
-    # time.sleep(2)
-    # robot.go_forward_fast()
-    # robot.pull_up()
-    # robot.pull_to_bridge_drop_off()
-    # robot.turn_arm_down()
-    # robot.go_forward_and_stop_after_duration(50, Robot.calculate_duration_length_in_mm(50, 400))  # todo: measure distance
-    # robot.celebrate(robot.found_pictogram, time.time() - start_time)
+    robot.go_forward_and_stop_after_duration(70, Robot.calculate_duration_length_in_mm(70, 300))
+    robot.turn_and_find_pictogram(True)
+    robot.speaker.play_text(robot.found_pictogram)
+    robot.turn_and_find_stair_with_camera_and_ultrasonic_control()  # TODO: decide which methode should be used
+    robot.go_forward_and_get_distance()
+    robot.do_alignment()
+    robot.go_to_drop_off_position()
+    robot.let_bridge_down()
+    robot.go_forward_and_stop_after_duration(50, 1)
+    robot.go_forward_and_stop_after_duration(400, 1)
+    robot.go_backward_and_stop_after_duration(400, 1)
+    robot.go_backward_and_stop_after_duration(50, 1)
+    robot.let_socket_down()
+    time.sleep(2)
+    robot.go_forward_fast()
+    robot.pull_up()
+    robot.pull_to_bridge_drop_off()
+    robot.turn_arm_down()
+    robot.go_forward_and_stop_after_duration(50, Robot.calculate_duration_length_in_mm(50, 400))  # todo: measure distance
+    robot.celebrate(robot.found_pictogram, time.time() - start_time)
     robot.dispose()
 
 except Exception as ex:
